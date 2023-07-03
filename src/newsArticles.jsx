43dd718013fa1e5d1a937940357c70614d0bac85
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2023-07-03 09:18:30 +0800

    Initialization Commit

---
+++ src/newsArticles.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './newsArticle.css'
+
+function NewsArticles() {
+    return (
+      <div className="news-articles-container">
+        News Articles
+      </div>
+    )
+  }
+  
+export default NewsArticles
