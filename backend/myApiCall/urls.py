7fccb129024549da9abcbeb2f446e9d5a2b432e4
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2023-07-04 05:26:31 +0800

    feat: multiple delete articles and integration of frontend and backend frameworks

--- backend/myApiCall/urls.py
+++ backend/myApiCall/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('', get_routes),
     path('get-all-articles', get_all_articles),
+    path('delete-article/<str:pk>/', delete_article),
+    path('delete-multiple-articles/', delete_multiple_articles)
 ]
