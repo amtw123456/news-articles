4039bb80e39227a34a7c08c3a88c4f825858dbbb
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2023-07-04 09:07:08 +0800

    chore: changed api call links

--- src/articles.jsx
+++ src/articles.jsx
@@ -23,7 +23,7 @@ function ArticlePosts() {
   };
 
   const deleteSingleArticle = async  (id) => {
-    const response = await fetch(`http://127.0.0.1:8000/delete-article/${id}/`, {
+    const response = await fetch(`https://news-articles-backend-server.vercel.app/get-all-articles/delete-article/${id}/`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
